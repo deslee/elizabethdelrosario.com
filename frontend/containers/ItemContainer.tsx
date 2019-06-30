85255a8999d6bdb37579846343db218c1b8e5bc7
Desmond Lee <desmondclee@gmail.com>
2019-06-30 17:46:39 -0500

    todos

--- frontend/containers/ItemContainer.tsx
+++ frontend/containers/ItemContainer.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout/Layout";
 import Error from 'next/error';
 import { PostFragment, PageFragment, PostCollectionFragment, SiteSettingsFragment, Maybe } from "../graphql";
 import Item from "../components/Item/Item";
-import { makeStyles, Divider } from "@material-ui/core";
+import { makeStyles, Divider, LinearProgress } from "@material-ui/core";
 
 interface ComponentProps {
     item?: Maybe<PostFragment> | Maybe<PageFragment> | Maybe<PostCollectionFragment>
@@ -24,24 +24,21 @@ const ItemContainer = (props: Props) => {
     const classes = useStyles();
 
     if (loading) {
-        // TODO: make loading screen
         return <Layout siteSettings={siteSettings}>
-            loading
+            <LinearProgress />
         </Layout>
     }
 
     if (!item) {
         return <Error statusCode={404} />
     }
-    
-    return <Layout siteSettings={siteSettings} title={item.title} >
-        <>
-            <Item item={item} />
-            {item.__typename === 'PostCollection' && (item.posts || []).map((post, idx) => post ? <Fragment key={idx}>
-                <Item item={post} />
-                { idx !== (item.posts || []).length - 1 && <Divider className={classes.divider} />}
-            </Fragment> : null)}
-        </>
+
+    return <Layout siteSettings={siteSettings} title={item.title}>
+        <Item item={item} />
+        {item.__typename === 'PostCollection' && (item.posts || []).map((post, idx) => post ? <Fragment key={idx}>
+            <Item item={post} />
+            {idx !== (item.posts || []).length - 1 && <Divider className={classes.divider} />}
+        </Fragment> : null)}
     </Layout>
 }
 
