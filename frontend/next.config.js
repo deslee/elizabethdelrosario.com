ee69c514d4d220e9c96c0734f7b534637c18696d
Desmond Lee <desmondclee@gmail.com>
2019-07-02 23:35:49 -0500

    target serverless

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,15 @@
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
-const sanityClient = require('@sanity/client')
+// const sanityClient = require('@sanity/client')
 
-const projectId = 'sj7jy8qa';
-const dataset = 'production'
+// const projectId = 'sj7jy8qa';
+// const dataset = 'production'
 
-const client = sanityClient({
-  projectId,
-  dataset,
-  useCdn: false
-})
+// const client = sanityClient({
+//   projectId,
+//   dataset,
+//   useCdn: false
+// })
 
 module.exports = withCSS(withTypescript({
   webpack(config, options) {
@@ -18,28 +18,29 @@ module.exports = withCSS(withTypescript({
     })
     return config
   },
-  exportPathMap: async function(defaultPageMap) {
-    const slugs = await client.fetch(`*[_type == 'post' || _type == 'page' || _type == 'postCollection'] {
-      slug {
-          current
-        }
-      }`)
+  // exportPathMap: async function(defaultPageMap) {
+  //   const slugs = await client.fetch(`*[_type == 'post' || _type == 'page' || _type == 'postCollection'] {
+  //     slug {
+  //         current
+  //       }
+  //     }`)
 
-    const filteredSlugs = slugs.map(item => item && item.slug && item.slug.current).filter(i => i && i !== '/')
+  //   const filteredSlugs = slugs.map(item => item && item.slug && item.slug.current).filter(i => i && i !== '/')
 
-    const pathMap = filteredSlugs.map(slug => ({
-      [`/${slug}`]: {
-        page: '/',
-        query: {
-          slug: slug
-        }
-      }
-    }))
-      .concat([{ ['/']: { page: '/' } }])
-      .reduce((pathMap, path) => Object.assign(pathMap, path), {});
+  //   const pathMap = filteredSlugs.map(slug => ({
+  //     [`/${slug}`]: {
+  //       page: '/',
+  //       query: {
+  //         slug: slug
+  //       }
+  //     }
+  //   }))
+  //     .concat([{ ['/']: { page: '/' } }])
+  //     .reduce((pathMap, path) => Object.assign(pathMap, path), {});
 
-    console.log(pathMap)
-    return pathMap
-  }
+  //   console.log(pathMap)
+  //   return pathMap
+  // }
   /* config options here */
+  target: 'serverless'
 }))
