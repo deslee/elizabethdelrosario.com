196beff9bd8bfa99b2d54ed85bee1f096ffe8ee6
Desmond Lee <desmondclee@gmail.com>
2019-07-02 23:23:40 -0500

    modify build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,15 @@
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
+const sanityClient = require('@sanity/client')
+
+const projectId = 'sj7jy8qa';
+const dataset = 'production'
+
+const client = sanityClient({
+  projectId,
+  dataset,
+  useCdn: false
+})
 
 module.exports = withCSS(withTypescript({
   webpack(config, options) {
@@ -9,10 +19,27 @@ module.exports = withCSS(withTypescript({
     return config
   },
   exportPathMap: async function(defaultPageMap) {
-    return {
-      '/': { page: '/' },
-      '/bio': { page: '/', query: { slug: 'bio' } }
-    }
+    const slugs = await client.fetch(`*[_type == 'post' || _type == 'page' || _type == 'postCollection'] {
+      slug {
+          current
+        }
+      }`)
+
+    const filteredSlugs = slugs.map(item => item && item.slug && item.slug.current).filter(i => i && i !== '/')
+
+    const pathMap = filteredSlugs.map(slug => ({
+      [`/${slug}`]: {
+        page: '/',
+        query: {
+          slug: slug
+        }
+      }
+    }))
+      .concat([{ ['/']: { page: '/' } }])
+      .reduce((pathMap, path) => Object.assign(pathMap, path), {});
+
+    console.log(pathMap)
+    return pathMap
   }
   /* config options here */
 }))
